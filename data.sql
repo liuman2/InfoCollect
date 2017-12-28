ac5a8199d62d998696e0688c3afa1a412e2887ed
liuman2 <liuman2@163.com>
2017-12-29 00:01:06 +0800

    update

--- data.sql
+++ data.sql
@@ -10,10 +10,47 @@ Target Server Type    : MYSQL
 Target Server Version : 50712
 File Encoding         : 65001
 
-Date: 2017-12-28 22:03:34
+Date: 2017-12-29 00:00:47
 */
 
 SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for profile
+-- ----------------------------
+DROP TABLE IF EXISTS `profile`;
+CREATE TABLE `profile` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `name` varchar(50) DEFAULT NULL,
+  `gender` tinyint(1) DEFAULT NULL,
+  `card_no` varchar(100) DEFAULT NULL,
+  `card_front` varchar(255) DEFAULT NULL,
+  `card_back` varchar(255) DEFAULT NULL,
+  `card_hold` varchar(255) DEFAULT NULL,
+  `province` varchar(50) DEFAULT NULL,
+  `city` varchar(50) DEFAULT NULL,
+  `county` varchar(50) DEFAULT NULL,
+  `address` varchar(255) DEFAULT NULL,
+  `contact` varchar(30) DEFAULT NULL,
+  `tel` varchar(20) DEFAULT NULL,
+  `date_created` datetime DEFAULT NULL,
+  `date_modify` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for sms
+-- ----------------------------
+DROP TABLE IF EXISTS `sms`;
+CREATE TABLE `sms` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `mobile` varchar(20) DEFAULT NULL,
+  `code` varchar(8) DEFAULT NULL,
+  `type` tinyint(1) DEFAULT NULL,
+  `date_created` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Table structure for user
@@ -28,4 +65,4 @@ CREATE TABLE `user` (
   `status` tinyint(1) DEFAULT NULL,
   `date_created` datetime DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8;
