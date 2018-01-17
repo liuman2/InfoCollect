41b6b1bba0ec005b8e21d79f1171149ea0e31845
liuman2 <liuman2@163.com>
2018-01-17 08:19:13 +0800

    update

--- app/service/profile.js
+++ app/service/profile.js
@@ -6,29 +6,28 @@ const Service = require('egg').Service;
 class ProfileService extends Service {
   async save(req) {
     let result = null;
-    const reqProfile = req.profile;
 
     const profile = await this.app.mysql.get('profile', { user_id: req.user_id });
     if (profile === null) {
-      reqProfile.status = 0;
-      reqProfile.date_created = new Date().toLocaleString();
-      result = await this.app.mysql.insert('profile', reqProfile);
+      req.status = 0;
+      req.date_created = new Date().toLocaleString();
+      result = await this.app.mysql.insert('profile', req);
     } else {
-      profile.name = reqProfile.name;
-      profile.gender = reqProfile.gender;
-      profile.card_no = reqProfile.card_no;
-      profile.card_front = reqProfile.card_front;
-      profile.card_back = reqProfile.card_back;
-      profile.card_hold = reqProfile.card_hold;
-      profile.self_photo = reqProfile.self_photo;
-      profile.address = reqProfile.address;
-      profile.contact = reqProfile.contact;
-      profile.tel = reqProfile.tel;
+      profile.name = req.name;
+      profile.gender = req.gender;
+      profile.card_no = req.card_no;
+      profile.card_front = req.card_front;
+      profile.card_back = req.card_back;
+      profile.card_hold = req.card_hold;
+      profile.self_photo = req.self_photo;
+      profile.address = req.address;
+      profile.contact = req.contact;
+      profile.tel = req.tel;
       profile.province = null;
       profile.city = null;
       profile.county = null;
       profile.date_modify = new Date().toLocaleString();
-
+      profile.protocol = req.protocol;
       result = await this.app.mysql.update('profile', profile);
     }
 
